45dae6648470ae4b01ecbc7b034467d215b8eef8
Young Bae <bae.young@nhn.com>
2018-02-02 15:14:37 +0900

    Loading states
    insert 'loading' sentence if the movie state is empty

--- src/App.js
+++ src/App.js
@@ -2,48 +2,50 @@ import React, {Component} from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const movies = [
-  {
-    title: "Matrix Reloaded",
-    poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2uQY1VHHyubG6L1LXEaXcUMTHSlPJ3PS9-fO5Xx5ivxY61mIe",
-  },
-  {
-    title: "Baby Driver",
-    poster: "https://pbs.twimg.com/media/C_j4udWXYAEQ_Jv.jpg",
-  },
-  {
-    title: "Paris Je T'aime",
-    poster: "http://image.yes24.com/blogimage/blog/d/a/dahamida/20170213230407156062.jpg",
-  },
-  {
-    title: "Harry Potter and the Prisoner of Azkaban",
-    poster: "https://www.harrypottermovieposters.com/wp-content/uploads/2014/05/harry-potter-and-the-prisoner-of-azkaban-movie-poster-2004-1020288138.jpg",
-  }
-]
-
 class App extends Component {
-  state = {
-    greeting: 'hello',
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: [],
+    };
   }
 
   componentDidMount() {
     setTimeout(() => {
       this.setState({
-        greeting: 'Hello again'
+        movies: [
+          {
+            title: "Matrix Reloaded",
+            poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2uQY1VHHyubG6L1LXEaXcUMTHSlPJ3PS9-fO5Xx5ivxY61mIe",
+          },
+          {
+            title: "Baby Driver",
+            poster: "https://pbs.twimg.com/media/C_j4udWXYAEQ_Jv.jpg",
+          },
+          {
+            title: "Paris Je T'aime",
+            poster: "http://image.yes24.com/blogimage/blog/d/a/dahamida/20170213230407156062.jpg",
+          },
+          {
+            title: "Harry Potter and the Prisoner of Azkaban",
+            poster: "https://www.harrypottermovieposters.com/wp-content/uploads/2014/05/harry-potter-and-the-prisoner-of-azkaban-movie-poster-2004-1020288138.jpg",
+          },
+        ]
       })
     }, 5000);
+  }
+
+  _renderMovies = () => {
+    const movies = this.state.movies.map((item, index) => {
+      return <Movie title={item.title} poster={item.poster} key={`movie_${index+1}`} />
+    });
+    return movies;
   }
 
   render() {
     return (
       <div className="App">
-        <h1>Movie App</h1>
-        <span>{this.state.greeting}</span>
-        <ul>
-          {movies.map((item, index) => {
-            return <Movie title={item.title} poster={item.poster} key={`movie_${index+1}`} />
-          })}
-        </ul>
+        {this.state.movies.length > 0 ? this._renderMovies() : 'Loading'}
       </div>
     );
   }
