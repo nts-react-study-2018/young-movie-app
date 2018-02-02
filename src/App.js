3f389242e17dddde3fbe2149cb2c27f8f112aa9d
Young Bae <bae.young@nhn.com>
2018-02-02 16:25:11 +0900

    AJAX on react

--- src/App.js
+++ src/App.js
@@ -11,28 +11,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        movies: [
-          {
-            title: "Matrix Reloaded",
-            poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2uQY1VHHyubG6L1LXEaXcUMTHSlPJ3PS9-fO5Xx5ivxY61mIe",
-          },
-          {
-            title: "Baby Driver",
-            poster: "https://pbs.twimg.com/media/C_j4udWXYAEQ_Jv.jpg",
-          },
-          {
-            title: "Paris Je T'aime",
-            poster: "http://image.yes24.com/blogimage/blog/d/a/dahamida/20170213230407156062.jpg",
-          },
-          {
-            title: "Harry Potter and the Prisoner of Azkaban",
-            poster: "https://www.harrypottermovieposters.com/wp-content/uploads/2014/05/harry-potter-and-the-prisoner-of-azkaban-movie-poster-2004-1020288138.jpg",
-          },
-        ]
-      })
-    }, 5000);
+    console.log(fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=52ee229af86240d8b73edfe0864260be&language=en-US&page=1'));
   }
 
   _renderMovies = () => {
